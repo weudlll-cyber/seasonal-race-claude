8eb1994c4db3e2e8e8b72834a5645b0bddd00e95
Weudl <weudlll@gmail.com>
2026-06-09 00:51:31 +0200

    refactor(client): tag timeout error instead of message-substring check [H-07]

--- client/src/services/apiClient.js
+++ client/src/services/apiClient.js
@@ -15,20 +15,20 @@
 
 const TIMEOUT_MS = 8000;
 
+const UNREACHABLE_MSG =
+  'Server not reachable. Check that the backend is running (docker compose up in the project root), then try again.';
+
+class TimeoutError extends Error {
+  constructor() {
+    super(UNREACHABLE_MSG);
+    this.code = 'TIMEOUT';
+  }
+}
+
 export function withTimeout(promise, ms) {
   return Promise.race([
     promise,
-    new Promise((_, reject) =>
-      setTimeout(
-        () =>
-          reject(
-            new Error(
-              'Server not reachable. Check that the backend is running (docker compose up in the project root), then try again.'
-            )
-          ),
-        ms
-      )
-    ),
+    new Promise((_, reject) => setTimeout(() => reject(new TimeoutError()), ms)),
   ]);
 }
 
@@ -37,11 +37,7 @@ export async function apiCall(url, options = {}) {
   try {
     res = await withTimeout(fetch(url, options), TIMEOUT_MS);
   } catch (err) {
-    throw new Error(
-      err.message.includes('docker compose')
-        ? err.message
-        : 'Server not reachable. Check that the backend is running (docker compose up in the project root), then try again.'
-    );
+    throw new Error(err.code === 'TIMEOUT' ? err.message : UNREACHABLE_MSG);
   }
   if (!res.ok) {
     let errMsg = `HTTP ${res.status}`;
